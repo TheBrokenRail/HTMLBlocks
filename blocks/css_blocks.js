24ead0f5573705fe04f7451f150c7564409b1620
Travis CI <travis@example.org>
2018-01-28 16:35:24 +0000

    Deploy to Github Pages

---
+++ blocks/css_blocks.js
@@ -0,0 +1,369 @@
+var ScratchBlocks = window.ScratchBlocks;
+ScratchBlocks.Categories.css = "css";
+
+ScratchBlocks.Blocks['css'] = {
+    init: function () {
+        this.jsonInit({
+            "id": "css",
+            "message0": "CSS",
+            "message1": "%1",
+            "args1": [
+                {
+                    "type": "input_statement",
+                    "name": "SUBSTACK"
+                }
+            ],
+            "inputsInline": true,
+            "category": ScratchBlocks.Categories.css,
+            "colour": ScratchBlocks.Colours.css.primary,
+            "colourSecondary": ScratchBlocks.Colours.css.secondary,
+            "colourTertiary": ScratchBlocks.Colours.css.tertiary
+        });
+    }
+};
+
+ScratchBlocks.JavaScript['css'] = function (block) {
+    return "var style = document.createElement('STYLE');" + 
+        ScratchBlocks.JavaScript.statementToCode(block, 'SUBSTACK');
+};
+
+ScratchBlocks.Blocks['css_selector'] = {
+    init: function () {
+        this.jsonInit({
+            "id": "css_selector",
+            "message0": "selector %1",
+            "message1": "%1",
+            "args0": [
+                {
+                    "type": "input_value",
+                    "name": "SELECTOR"
+                }
+            ],
+            "args1": [
+                {
+                    "type": "input_statement",
+                    "name": "SUBSTACK"
+                }
+            ],
+            "inputsInline": true,
+            "previousStatement": null,
+            "nextStatement": null,
+            "category": ScratchBlocks.Categories.css,
+            "colour": ScratchBlocks.Colours.css.primary,
+            "colourSecondary": ScratchBlocks.Colours.css.secondary,
+            "colourTertiary": ScratchBlocks.Colours.css.tertiary
+        });
+    },
+    onchange: function () {
+        function getTop(block) {
+            if (block.getSurroundParent()) {
+                return getTop(block.getSurroundParent());
+            }
+            return block;
+        }
+        var realTop = getTop(this);
+        var top = this.getSurroundParent() || this;
+        if (this.getSurroundParent() && this.getSurroundParent().disabled) {
+            this.disabled = true;
+        } else if (!realTop.previousConnection && top.type === "css" && realTop.type === "css") {
+            this.disabled = false;
+        } else if (realTop.previousConnection) {
+            this.disabled = false;
+        } else {
+            this.disabled = true;
+        }
+        var inputs = this.inputList;
+        for (i = 0; i < inputs.length; i++) {
+            if (inputs[i].type !== 3 && inputs[i].connection) {
+                if (inputs[i].connection.targetBlock()) {
+                    if (inputs[i].connection.targetBlock().disabled && inputs[i].connection.targetBlock().isName) {
+                        this.disabled = true;
+                    }
+                }
+            }
+        }
+        if (this.disabled) {
+            if (!this.isInsertionMarker()) this.setOpacity(0.45);
+        } else {
+            if (!this.isInsertionMarker()) this.setOpacity(1);
+        }
+    }
+};
+
+ScratchBlocks.JavaScript['css_selector'] = function (block) {
+    return "style.appendChild(document.createTextNode('" + 
+        ScratchBlocks.JavaScript.valueToCode(block, 'SELECTOR') + 
+        " {" + 
+        ScratchBlocks.JavaScript.statementToCode(block, 'SUBSTACK') + 
+        "}'));";
+};
+
+ScratchBlocks.Blocks['css_attribute'] = {
+    init: function () {
+        this.jsonInit({
+            "id": "css_attribute",
+            "message0": "set attribute %1 to %2",
+            "args0": [
+                {
+                    "type": "input_value",
+                    "name": "ATTRIBUTE"
+                },
+                {
+                    "type": "input_value",
+                    "name": "VALUE"
+                }
+            ],
+            "inputsInline": true,
+            "previousStatement": null,
+            "nextStatement": null,
+            "category": ScratchBlocks.Categories.css,
+            "colour": ScratchBlocks.Colours.css.primary,
+            "colourSecondary": ScratchBlocks.Colours.css.secondary,
+            "colourTertiary": ScratchBlocks.Colours.css.tertiary
+        });
+    },
+    onchange: function () {
+        function getTop(block) {
+            if (block.getSurroundParent()) {
+                return getTop(block.getSurroundParent());
+            }
+            return block;
+        }
+        var realTop = getTop(this);
+        var top = this.getSurroundParent() || this;
+        if (this.getSurroundParent() && this.getSurroundParent().disabled) {
+            this.disabled = true;
+        } else if (!realTop.previousConnection && top.type === "css_selector" && realTop.type === "css") {
+            this.disabled = false;
+        } else if (realTop.previousConnection) {
+            this.disabled = false;
+        } else {
+            this.disabled = true;
+        }
+        var inputs = this.inputList;
+        for (i = 0; i < inputs.length; i++) {
+            if (inputs[i].type !== 3 && inputs[i].connection) {
+                if (inputs[i].connection.targetBlock()) {
+                    if (inputs[i].connection.targetBlock().disabled && inputs[i].connection.targetBlock().isName) {
+                        this.disabled = true;
+                    }
+                }
+            }
+        }
+        if (this.disabled) {
+            if (!this.isInsertionMarker()) this.setOpacity(0.45);
+        } else {
+            if (!this.isInsertionMarker()) this.setOpacity(1);
+        }
+    }
+};
+
+ScratchBlocks.JavaScript['css_attribute'] = function (block) {
+    return "  " + 
+        ScratchBlocks.JavaScript.valueToCode(block, 'ATTRIBUTE') + 
+        ": " + 
+        ScratchBlocks.JavaScript.valueToCode(block, 'VALUE') + 
+        ";";
+};
+
+ScratchBlocks.Blocks["css_addon_selector_id"] = {
+    init: function() {
+        this.isName = true;
+        this.jsonInit({
+            "id": "css_selector_id",
+            "message0": "id %1",
+            "args0": [
+                {
+                    "type": "input_value",
+                    "name": "ID"
+                }
+             ],
+            "inputsInline": true,
+            "category": ScratchBlocks.Categories.css,
+            "colour": ScratchBlocks.Colours.css.primary,
+            "colourSecondary": ScratchBlocks.Colours.css.secondary,
+            "colourTertiary": ScratchBlocks.Colours.css.tertiary,
+            "outputShape": ScratchBlocks.OUTPUT_SHAPE_ROUND,
+            "output": "String"
+        });
+    },
+    onchange: function() {
+        this.disabled = false;
+        if (this.getParent()) this.getParent().onchange();
+        if (this.getParent() && this.getParent().type.startsWith("css_selector") && !this.getParent().disabled && this.getParent().getInputTargetBlock("SELECTOR") === this) {
+            this.disabled = false;
+        } else if (!this.getParent()) {
+            this.disabled = false;
+        } else {
+            this.disabled = true;
+        }
+        var inputs = this.inputList;
+        for (i = 0; i < inputs.length; i++) {
+            if (inputs[i].type !== 3 && inputs[i].connection) {
+                if (inputs[i].connection.targetBlock()) {
+                    if (inputs[i].connection.targetBlock().disabled && inputs[i].connection.targetBlock().isName) {
+                        this.disabled = true;
+                    }
+                }
+            }
+        }
+        if (this.getParent()) this.getParent().onchange();
+        if (this.disabled) {
+            if (!this.isInsertionMarker()) this.setOpacity(0.45);
+        } else {
+            if (!this.isInsertionMarker()) this.setOpacity(1);
+        }
+    }
+};
+
+ScratchBlocks.JavaScript["css_addon_selector_id"] = function(block) {
+    return ["#" + ScratchBlocks.JavaScript.valueToCode(block, 'ID')];
+};
+
+ScratchBlocks.Blocks["css_addon_selector_class"] = {
+    init: function() {
+        this.isName = true;
+        this.jsonInit({
+            "id": "css_selector_class",
+            "message0": "class %1",
+            "args0": [
+                {
+                    "type": "input_value",
+                    "name": "CLASS"
+                }
+             ],
+            "inputsInline": true,
+            "category": ScratchBlocks.Categories.css,
+            "colour": ScratchBlocks.Colours.css.primary,
+            "colourSecondary": ScratchBlocks.Colours.css.secondary,
+            "colourTertiary": ScratchBlocks.Colours.css.tertiary,
+            "outputShape": ScratchBlocks.OUTPUT_SHAPE_ROUND,
+            "output": "String"
+        });
+    },
+    onchange: function() {
+        this.disabled = false;
+        if (this.getParent()) this.getParent().onchange();
+        if (this.getParent() && this.getParent().type.startsWith("css_selector") && !this.getParent().disabled && this.getParent().getInputTargetBlock("SELECTOR") === this) {
+            this.disabled = false;
+        } else if (!this.getParent()) {
+            this.disabled = false;
+        } else {
+            this.disabled = true;
+        }
+        var inputs = this.inputList;
+        for (i = 0; i < inputs.length; i++) {
+            if (inputs[i].type !== 3 && inputs[i].connection) {
+                if (inputs[i].connection.targetBlock()) {
+                    if (inputs[i].connection.targetBlock().disabled && inputs[i].connection.targetBlock().isName) {
+                        this.disabled = true;
+                    }
+                }
+            }
+        }
+        if (this.getParent()) this.getParent().onchange();
+        if (this.disabled) {
+            if (!this.isInsertionMarker()) this.setOpacity(0.45);
+        } else {
+            if (!this.isInsertionMarker()) this.setOpacity(1);
+        }
+    }
+};
+
+ScratchBlocks.JavaScript["css_addon_selector_class"] = function(block) {
+    return ["." + ScratchBlocks.JavaScript.valueToCode(block, 'CLASS')];
+};
+
+ScratchBlocks.Blocks["css_selector_pseudo_class"] = {
+    init: function() {
+        this.isName = true;
+        this.jsonInit({
+            "id": "css_selector_pseudo_class",
+            "message0": "pseudo class %1 of %2",
+            "args0": [
+                {
+                    "type": "field_dropdown",
+                    "name": "PSEUDO_CLASS",
+                    "options": [
+                        ["active", "active"],
+                        ["any", "any"],
+                        ["any-link", "any-link"],
+                        ["checked", "checked"],
+                        ["default", "default"],
+                        ["disabled", "disabled"],
+                        ["empty", "empty"],
+                        ["enabled", "enabled"],
+                        ["first", "first"],
+                        ["first-child", "first-child"],
+                        ["first-of-type", "first-of-type"],
+                        ["fullscreen", "fullscreen"],
+                        ["focus", "focus"],
+                        ["hover", "hover"],
+                        ["indeterminate", "indeterminate"],
+                        ["in-range", "in-range"],
+                        ["invalid", "invalid"],
+                        ["last-child", "last-child"],
+                        ["last-of-type", "last-of-type"],
+                        ["left", "left"],
+                        ["link", "link"],
+                        ["only-child", "only-child"],
+                        ["only-of-type", "only-of-type"],
+                        ["optional", "optional"],
+                        ["out-of-range", "out-of-range"],
+                        ["read-only", "read-only"],
+                        ["read-write", "read-write"],
+                        ["required", "required"],
+                        ["right", "right"],
+                        ["root", "root"],
+                        ["scope", "scope"],
+                        ["target", "target"],
+                        ["valid", "valid"],
+                        ["visited", "visited"]
+                    ]
+                },
+                {
+                    "type": "input_value",
+                    "name": "SELECTOR"
+                }
+             ],
+            "inputsInline": true,
+            "category": ScratchBlocks.Categories.css,
+            "colour": ScratchBlocks.Colours.css.primary,
+            "colourSecondary": ScratchBlocks.Colours.css.secondary,
+            "colourTertiary": ScratchBlocks.Colours.css.tertiary,
+            "outputShape": ScratchBlocks.OUTPUT_SHAPE_ROUND,
+            "output": "String"
+        });
+    },
+    onchange: function() {
+        this.disabled = false;
+        if (this.getParent()) this.getParent().onchange();
+        if (this.getParent() && this.getParent().type.startsWith("css_selector") && !this.getParent().disabled && this.getParent().getInputTargetBlock("SELECTOR") === this) {
+            this.disabled = false;
+        } else if (!this.getParent()) {
+            this.disabled = false;
+        } else {
+            this.disabled = true;
+        }
+        var inputs = this.inputList;
+        for (i = 0; i < inputs.length; i++) {
+            if (inputs[i].type !== 3 && inputs[i].connection) {
+                if (inputs[i].connection.targetBlock()) {
+                    if (inputs[i].connection.targetBlock().disabled && inputs[i].connection.targetBlock().isName) {
+                        this.disabled = true;
+                    }
+                }
+            }
+        }
+        if (this.getParent()) this.getParent().onchange();
+        if (this.disabled) {
+            if (!this.isInsertionMarker()) this.setOpacity(0.45);
+        } else {
+            if (!this.isInsertionMarker()) this.setOpacity(1);
+        }
+    }
+};
+
+ScratchBlocks.JavaScript["css_selector_pseudo_class"] = function(block) {
+    return [ScratchBlocks.JavaScript.valueToCode(block, 'SELECTOR') + ":" + block.getFieldValue('PSEUDO_CLASS')];
+};
---
+++ blocks/html_blocks.js
@@ -0,0 +1,324 @@
+var ScratchBlocks = window.ScratchBlocks;
+
+ScratchBlocks.Categories.html = "html";
+
+ScratchBlocks.Blocks['html'] = {
+    init: function() {
+        this.jsonInit({
+            "id": "html",
+            "message0": "HTML",
+            "message1": "%1",
+            "args1": [
+                {
+                    "type": "input_statement",
+                    "name": "SUBSTACK"
+                }
+            ],
+            "inputsInline": true,
+            "category": ScratchBlocks.Categories.html,
+            "colour": ScratchBlocks.Colours.html.primary,
+            "colourSecondary": ScratchBlocks.Colours.html.secondary,
+            "colourTertiary": ScratchBlocks.Colours.html.tertiary
+        });
+    },
+    onchange: function () {
+        if (!this.disabled) {
+            this.svgGroup_.onmouseover = this.onmouseover.bind(this);
+        } else {
+            this.svgGroup_.onmouseover = null;
+        }
+        this.svgGroup_.onmouseout = this.onmouseout.bind(this);
+    },
+    onmouseover: function (e) {
+        if (window.highlightElement) {
+            window.highlightElement.parentNode.removeChild(window.highlightElement);
+            window.highlightElement = null;
+        }
+        var preview = document.getElementById("preview").contentDocument;
+        var highlight = preview.createElement("DIV");
+        highlight.style.position = "absolute";
+        highlight.style.backgroundColor = "rgba(0,255,255,0.5)";
+        highlight.style.width = "100%";
+        highlight.style.height = "100%";
+        highlight.style.top = "0px";
+        highlight.style.left = "0px";
+        window.highlightElement = highlight;
+        preview.body.appendChild(window.highlightElement);
+        e.stopPropagation();
+    },
+    onmouseout: function (e) {
+        if (window.highlightElement) {
+            window.highlightElement.parentNode.removeChild(window.highlightElement);
+            window.highlightElement = null;
+        }
+        e.stopPropagation();
+    }
+};
+
+ScratchBlocks.JavaScript['html'] = function (block) {
+    return "var element = document.createElement('HTML');" +
+        ScratchBlocks.JavaScript.statementToCode(block, 'SUBSTACK');
+};
+
+ScratchBlocks.Blocks['html_element'] = {
+    init: function() {
+        this.jsonInit({
+            "id": "html_element",
+            "message0": "element %1",
+            "message1": "%1",
+            "args0": [
+                {
+                    "type": "input_value",
+                    "name": "ELEMENT"
+                }
+            ],
+            "args1": [
+                {
+                    "type": "input_statement",
+                    "name": "SUBSTACK"
+                }
+            ],
+            "inputsInline": true,
+            "previousStatement": null,
+            "nextStatement": null,
+            "category": ScratchBlocks.Categories.html,
+            "colour": ScratchBlocks.Colours.html.primary,
+            "colourSecondary": ScratchBlocks.Colours.html.secondary,
+            "colourTertiary": ScratchBlocks.Colours.html.tertiary
+        });
+    },
+    onchange: function () {
+        function getTop(block) {
+            if (block.getSurroundParent()) {
+                return getTop(block.getSurroundParent());
+            }
+            return block;
+        }
+        var top = getTop(this);
+        if (this.getSurroundParent() && this.getSurroundParent().disabled) {
+            this.disabled = true;
+        } else if (!top.previousConnection && top.type === "html") {
+            this.disabled = false;
+        } else if (top.previousConnection) {
+            this.disabled = false;
+        } else {
+            this.disabled = true;
+        }
+        var inputs = this.inputList;
+        for (i = 0; i < inputs.length; i++) {
+            if (inputs[i].type !== 3 && inputs[i].connection) {
+                if (inputs[i].connection.targetBlock()) {
+                    if (inputs[i].connection.targetBlock().disabled && inputs[i].connection.targetBlock().isName) {
+                        this.disabled = true;
+                    }
+                }
+            }
+        }
+        if (this.disabled) {
+            if (!this.isInsertionMarker()) this.setOpacity(0.45);
+        } else {
+            if (!this.isInsertionMarker()) this.setOpacity(1);
+        }
+        if (!this.disabled) {
+            this.svgGroup_.onmouseover = this.onmouseover.bind(this);
+        } else {
+            this.svgGroup_.onmouseover = null;
+        }
+        this.svgGroup_.onmouseout = this.onmouseout.bind(this);
+    },
+    onmouseover: function (e) {
+        if (window.highlightElement) {
+            window.highlightElement.parentNode.removeChild(window.highlightElement);
+            window.highlightElement = null;
+        }
+        var preview = document.getElementById("preview").contentDocument;
+        var elements = preview.querySelectorAll("*");
+        for (i = 0; i < elements.length; i++) {
+            if (elements[i].getAttribute("data-block-id-debug") === this.id) {
+                var highlight = preview.createElement("DIV");
+                highlight.style.position = "absolute";
+                highlight.style.backgroundColor = "rgba(0,255,255,0.5)";
+                highlight.style.width = elements[i].offsetWidth + "px";
+                highlight.style.height = elements[i].offsetHeight + "px";
+                var elementData = elements[i].getBoundingClientRect();
+                highlight.style.top = elementData.top + "px";
+                highlight.style.left = elementData.left + "px";
+                window.highlightElement = highlight;
+                preview.body.appendChild(window.highlightElement);
+            }
+        }
+        e.stopPropagation();
+    },
+    onmouseout: function (e) {
+        if (window.highlightElement) {
+            window.highlightElement.parentNode.removeChild(window.highlightElement);
+            window.highlightElement = null;
+        }
+        e.stopPropagation();
+    }
+};
+
+ScratchBlocks.JavaScript['html_element'] = function (block) {
+    var element = ScratchBlocks.JavaScript.valueToCode(block, 'ELEMENT');
+    var debugText = "";
+    if (window.debugExport) {
+        debugText = "element.setAttribute('data-block-id-debug','" +
+        block.id +
+        "');";
+    }
+    var code = "var tag = 'DIV';try{document.createElement('" +
+        element +
+        "');tag = '" +
+        element +
+        "';" +
+        "}catch(e){}" +
+        "element.appendChild((function (element) {" +
+        debugText +
+        ScratchBlocks.JavaScript.statementToCode(block, 'SUBSTACK') +
+        "return element;})(document.createElement(tag)));";
+    return code;
+};
+
+ScratchBlocks.Blocks['html_text'] = {
+    init: function() {
+        this.jsonInit({
+            "id": "html_text",
+            "message0": "text %1",
+            "args0": [
+                {
+                    "type": "input_value",
+                    "name": "TEXT"
+                }
+            ],
+            "inputsInline": true,
+            "previousStatement": null,
+            "nextStatement": null,
+            "category": ScratchBlocks.Categories.html,
+            "colour": ScratchBlocks.Colours.html.primary,
+            "colourSecondary": ScratchBlocks.Colours.html.secondary,
+            "colourTertiary": ScratchBlocks.Colours.html.tertiary
+        });
+    },
+    onchange: function () {
+        function getTop(block) {
+            if (block.getSurroundParent()) {
+                return getTop(block.getSurroundParent());
+            }
+            return block;
+        }
+        var top = getTop(this);
+        if (this.getSurroundParent() && this.getSurroundParent().disabled) {
+            this.disabled = true;
+        } else if (!top.previousConnection && top.type === "html") {
+            this.disabled = false;
+        } else if (top.previousConnection) {
+            this.disabled = false;
+        } else {
+            this.disabled = true;
+        }
+        var inputs = this.inputList;
+        for (i = 0; i < inputs.length; i++) {
+            if (inputs[i].type !== 3 && inputs[i].connection) {
+                if (inputs[i].connection.targetBlock()) {
+                    if (inputs[i].connection.targetBlock().disabled && inputs[i].connection.targetBlock().isName) {
+                        this.disabled = true;
+                    }
+                }
+            }
+        }
+        if (this.disabled) {
+            if (!this.isInsertionMarker()) this.setOpacity(0.45);
+        } else {
+            if (!this.isInsertionMarker()) this.setOpacity(1);
+        }
+        if (!this.disabled && this.getSurroundParent()) {
+            this.svgGroup_.onmouseover = this.getSurroundParent().onmouseover.bind(this.getSurroundParent());
+        } else {
+            this.svgGroup_.onmouseover = null;
+        }
+        if (this.getSurroundParent()) {
+            this.svgGroup_.onmouseout = this.getSurroundParent().onmouseout.bind(this.getSurroundParent());
+        }
+    }
+};
+
+ScratchBlocks.JavaScript['html_text'] = function (block) {
+    return "element.appendChild(document.createTextNode('" + 
+        ScratchBlocks.JavaScript.valueToCode(block, 'TEXT') + 
+        "'));";
+};
+
+ScratchBlocks.Blocks['html_attribute'] = {
+    init: function() {
+        this.jsonInit({
+            "id": "html_attribute",
+            "message0": "set attribute %1 to %2",
+            "args0": [
+                {
+                    "type": "input_value",
+                    "name": "ATTRIBUTE"
+                },
+                {
+                    "type": "input_value",
+                    "name": "VALUE"
+                }
+            ],
+            "inputsInline": true,
+            "previousStatement": null,
+            "nextStatement": null,
+            "category": ScratchBlocks.Categories.html,
+            "colour": ScratchBlocks.Colours.html.primary,
+            "colourSecondary": ScratchBlocks.Colours.html.secondary,
+            "colourTertiary": ScratchBlocks.Colours.html.tertiary
+        });
+    },
+    onchange: function () {
+        function getTop(block) {
+            if (block.getSurroundParent()) {
+                return getTop(block.getSurroundParent());
+            }
+            return block;
+        }
+        var top = getTop(this);
+        if (this.getSurroundParent() && this.getSurroundParent().disabled) {
+            this.disabled = true;
+        } else if (!top.previousConnection && top.type === "html") {
+            this.disabled = false;
+        } else if (top.previousConnection) {
+            this.disabled = false;
+        } else {
+            this.disabled = true;
+        }
+        var inputs = this.inputList;
+        for (i = 0; i < inputs.length; i++) {
+            if (inputs[i].type !== 3 && inputs[i].connection) {
+                if (inputs[i].connection.targetBlock()) {
+                    if (inputs[i].connection.targetBlock().disabled && inputs[i].connection.targetBlock().isName) {
+                        this.disabled = true;
+                    }
+                }
+            }
+        }
+        if (this.disabled) {
+            if (!this.isInsertionMarker()) this.setOpacity(0.45);
+        } else {
+            if (!this.isInsertionMarker()) this.setOpacity(1);
+        }
+        if (!this.disabled && this.getSurroundParent()) {
+            this.svgGroup_.onmouseover = this.getSurroundParent().onmouseover.bind(this.getSurroundParent());
+        } else {
+            this.svgGroup_.onmouseover = null;
+        }
+        if (this.getSurroundParent()) {
+            this.svgGroup_.onmouseout = this.getSurroundParent().onmouseout.bind(this.getSurroundParent());
+        }
+    }
+};
+
+ScratchBlocks.JavaScript['html_attribute'] = function (block) {
+    return "try{element.setAttribute('" + 
+        ScratchBlocks.JavaScript.valueToCode(block, 'ATTRIBUTE') + 
+        "','" + 
+        ScratchBlocks.JavaScript.valueToCode(block, 'VALUE') + 
+        "');}catch(e){}";
+};
